534ebb72bbfa7e716e8d6cced89b9b1ebfee479d
arishavelle18 <arishavelle18@yahoo.com>
2023-05-06 05:38:20 +0800

    pre-deployment 0

--- config/application.rb
+++ config/application.rb
@@ -33,7 +33,7 @@ module GraphqlApi
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    config.assets.paths << Rails.root.join("app", "assets", "graphiql", "rails")
+    
 
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
