727a6914d7c2164ab50af91fecfdec74b6f16e90
arishavelle18 <arishavelle18@yahoo.com>
2023-05-06 13:37:27 +0800

    predeploy

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,5 +1,2 @@
 //= link graphiql/rails/application.css
 //= link graphiql/rails/application.js
-//= link_tree ../images
-//= link_directory ../javascripts .js
-//= link_directory ../stylesheets .css
--- config/application.rb
+++ config/application.rb
@@ -33,7 +33,8 @@ module GraphqlApi
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    
+    config.assets.precompile += %w( graphiql/rails/application.css graphiql/rails/application.js )
+
 
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
